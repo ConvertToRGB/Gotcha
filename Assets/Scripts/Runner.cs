22741913c09c5b1645d13a0c1443da0c2ec62a17
ConvertToRGB <ConvertToRGB@gmail.com>
2022-01-25 22:57:24 +0200

    create basic setup

---
+++ Assets/Scripts/Runner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner : MonoBehaviour
+{
+    float speed = 1f;
+    float firstRunSpeed = 2f;
+    bool firstRun = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (firstRun)
+        {
+            FirstRun();
+            
+        }
+        else
+        {
+            Run();
+        }
+    }
+    private void Run()
+    {
+        transform.position += transform.up * (Time.deltaTime * speed);
+    }
+
+    private void FirstRun()
+    {
+        if (transform.position.y < 0)
+        {
+            transform.position += transform.up * (Time.deltaTime * firstRunSpeed);
+        }
+        else
+        {
+            firstRun = false;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject runner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateRunner();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreateRunner()
+    {
+        Vector3 pos = new Vector3(0, -5, 0);
+        GameObject runnerObject = Instantiate(runner, pos, Quaternion.identity);
+    }
+}
