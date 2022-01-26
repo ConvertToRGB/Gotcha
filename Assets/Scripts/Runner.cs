54dedfc92bb30f696c4f9aabc3a02d2083507664
ConvertToRGB <ConvertToRGB@gmail.com>
2022-01-27 00:03:36 +0200

    spawn based on Time.time

--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Runner : MonoBehaviour
 {
-    float speed = 1f;
+    float speed = 5f;
+    float rotSpeed = 1f;
     float firstRunSpeed = 3f;
     bool firstRun = true;
     bool inProcess = false;
@@ -13,13 +14,13 @@ public class Runner : MonoBehaviour
     Vector3 startPosition;
     Vector3 randomPosition;
     Vector3 endPosition = new Vector3(0, 0, 0);
+
     [SerializeField]
     private AnimationCurve curve;
 
     private void Start()
     {
         startPosition = FindObjectOfType<Spawner>().GetSpriteStartingPosition();
-        Debug.Log("Start Position: " + startPosition);
     }
     
     // Update is called once per frame
@@ -34,7 +35,6 @@ public class Runner : MonoBehaviour
         {
             Run();
         }
-        Debug.Log("Process Status: " + inProcess);
     }
 
     private void Run()
@@ -66,7 +66,7 @@ public class Runner : MonoBehaviour
     {
         if (!inProcess)
         {
-            randomPosition =  new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0);
+            randomPosition =  new Vector3(Random.Range(-1.6f, 1.7f), Random.Range(-4f, 3f), 0);
         }
     }
     private void SetStartingPosition()
@@ -80,17 +80,27 @@ public class Runner : MonoBehaviour
     private void Move(Vector3 startPos, Vector3 targetPos)
     {
         inProcess = true;
+        //if (Vector3.Distance(transform.position, targetPos) < 0.3f)
         if (transform.position != targetPos)
         {
             elapsedTime += Time.deltaTime * speed;
             float percentageComplete = elapsedTime / desiredDuration;
             transform.position = Vector3.Lerp(startPos, targetPos, percentageComplete);
+            LookAt2D(targetPos);
         }
         else
         {
             inProcess = false;
+            elapsedTime = 0f;
         }
 
+    }
+
+    private void LookAt2D(Vector3 target)
+    {
+        Vector3 targetDirection = transform.position - target;
+        float angle = (Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg)+90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
 
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,6 +7,8 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField]
     GameObject runner;
+    float elapsedTime;
+    float counter = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +19,44 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        elapsedTime += Time.time;
+        Debug.Log("Current Elapsed Time: " + elapsedTime);
+        CreateRunnerInUpdate();
+        Debug.Log(Math.Round(elapsedTime/100, 0));
+
     }
 
     private void CreateRunner()
     {
         Vector3 pos = GetSpriteStartingPosition();
         GameObject runnerObject = Instantiate(runner, pos, Quaternion.identity);
+    }
+
+    private void CreateRunnerInUpdate()
+    {
+        float whenToStart = Mathf.Round(elapsedTime / 100);
+        double remain = Math.Round(elapsedTime / 100, 0) - Convert.ToDouble((100f*counter));
+        Debug.Log("REMAIN: " + remain + "ELAPSED TIME: " + elapsedTime);
+        
+        if (whenToStart > 5f)
+        {
+            if (remain > 0f)
+            {
+                counter += 1f;
+                CreateRunner();
+                elapsedTime = 0f;
+                //StartCoroutine(CrtRunner());
+                Debug.Log("ELAPSED TIME: " + elapsedTime);
+                Debug.Log("Остаток: " + remain);
+            }
+            
+        }
+    }
+
+    IEnumerator CrtRunner()
+    {
+        CreateRunner();
+        yield return new WaitForSeconds(5000f);
     }
 
     public Vector3 GetSpriteStartingPosition()
